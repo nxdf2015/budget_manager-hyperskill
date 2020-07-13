86d677177729606bda9833d14d9986ef9e7be1e8
xavier niquet <niquet_xavier2002@yahoo.fr>
2020-07-13 10:40:31 +0200

    stage 3

--- BudgetManager.java
+++ BudgetManager.java
@@ -1,11 +1,18 @@
 package budget;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static budget.Main.TypePurchase.ALL;
+import static java.util.stream.Collectors.toList;
 
 public class BudgetManager {
+
     private double income ;
-    private List<String> purchases;
+    private List<Purchase> purchases;
     private double total;
 
     public BudgetManager() {
@@ -17,20 +24,78 @@ public class BudgetManager {
     public void addIncome(int income){
         this.income += income;
     }
-    public void addPurchase(String name, double price){
-       purchases.add(String.format("%s $%f",name,price));
+
+    public void addPurchase(String name, Main.TypePurchase type, double price){
+       purchases.add(Purchase.of(name,type,price));
        total += price;
     }
 
-    public List<String> getPurchases(){
-       return purchases;
+    public List<Purchase> getPurchases(Main.TypePurchase type){
+        if (type == ALL){
+            return purchases;
+        }
+        else {
+            return purchases.stream()
+                    .filter(purchase -> purchase.getType() == type)
+                    .collect(toList());
+        }
     }
 
-    public double getTotal(){
-        return total;
+
+    public double getTotal(Main.TypePurchase type){
+        List<Purchase> data;
+        if (type == ALL) {
+
+            data = purchases;
+        }
+        else {
+           data = getPurchases(type);
+        }
+
+        return data.stream()
+                .map(Purchase::getPrice)
+                .reduce(0., (total, price) -> total + price);
+
+
     }
 
     public double getBalance(){
+        total = getTotal(ALL);
         return income - total;
+    }
+
+
+    public boolean isEmpty() {
+        return purchases.isEmpty();
+    }
+}
+
+
+class Purchase {
+    private String name;
+    private double price;
+    private Main.TypePurchase type;
+
+    public Purchase(String name, Main.TypePurchase type, double price) {
+        this.name = name;
+        this.type = type;
+        this.price = price;
+    }
+
+    public static Purchase of(String name, Main.TypePurchase type, double price){
+        return new Purchase(name,type,price);
+    }
+
+    public Main.TypePurchase getType() {
+        return type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s $%s",name,price);
     }
 }
--- Main.java
+++ Main.java
@@ -5,18 +5,22 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Main {
-
+    enum TypePurchase {
+        FOOD,CLOTHES,ENTERTAIRNMENT,OTHER,ALL;
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         BudgetManager budgetManager = new BudgetManager();
+        int typePurchase = 0 ;
         while(true){
             System.out.println("Choose your action:\n" +
                     "1) Add income\n" +
                     "2) Add purchase\n" +
                     "3) Show list of purchases\n" +
                     "4) Balance\n" +
-                    "0) Exit\n");
+                    "0) Exit");
             int option = Integer.parseInt(scanner.nextLine());
+            System.out.println();
             switch (option){
                 case 0:
                     System.out.println("Bye!");
@@ -28,22 +32,62 @@ public class Main {
                     budgetManager.addIncome(income);
                     break;
                 case 2 :
-                    System.out.println("Enter purchase name:");
-                    String name = scanner.nextLine();
-                    System.out.println("Enter its price:");
-                    double price = Double.parseDouble(scanner.nextLine());
-                    budgetManager.addPurchase(name,price);
+                    typePurchase=0;
+                    while(true) {
+                        System.out.println("Choose the type of purchase\n" +
+                                "1) Food\n" +
+                                "2) Clothes\n" +
+                                "3) Entertainment\n" +
+                                "4) Other\n" +
+                                "5) Back");
+                        typePurchase = Integer.parseInt(scanner.nextLine());
+
+                        if(typePurchase==5){
+                            break;
+                        }
+                        else {
+                            System.out.println();
+                        }
+
+                        System.out.println("Enter purchase name:");
+                        String name = scanner.nextLine();
+                        System.out.println("Enter its price:");
+                        double price = Double.parseDouble(scanner.nextLine());
+                        budgetManager.addPurchase(name,TypePurchase.values()[typePurchase-1], price);
+                        System.out.println();
+                    }
                     break;
                 case 3:
-                    if (budgetManager.getPurchases().isEmpty()){
+                    if (budgetManager.isEmpty()){
                         System.out.println("Purchase list is empty");
 
                     }
-                    else{
-                        budgetManager.getPurchases()
-                                .stream()
-                                .forEach(System.out::println);
-                        System.out.println("Total sum: $"+budgetManager.getTotal());
+                    else {
+                        typePurchase = 0;
+                        while (true) {
+                            System.out.println("Choose the type of purchases\n" +
+                                    "1) Food\n" +
+                                    "2) Clothes\n" +
+                                    "3) Entertainment\n" +
+                                    "4) Other\n" +
+                                    "5) All\n" +
+                                    "6) Back");
+                            typePurchase = Integer.parseInt(scanner.nextLine());
+
+                            if (typePurchase == 6){
+                                break;
+                            }
+                            else {
+                                System.out.println();
+                            }
+                            TypePurchase type = TypePurchase.values()[typePurchase-1];
+                            budgetManager.getPurchases(type)
+                                    .stream()
+                                    .forEach(System.out::println);
+                            System.out.println("Total sum: $" + budgetManager.getTotal(type));
+                            System.out.println();
+
+                        }
                     }
                     break;
                 case 4:
@@ -54,5 +98,15 @@ public class Main {
             System.out.println();
         }
     }
-
+//        Scanner scanner = new Scanner(System.in);
+//         double total= 0 ;
+//
+//        while(scanner.hasNext()){
+//            String line = scanner.nextLine();
+//            System.out.println(line);
+//            int i = line.lastIndexOf('$');
+//            total += Double.parseDouble(line.substring(i+1,line.length()));
+//        }
+//        System.out.println("Total: $"+total);
+//    }
 }
